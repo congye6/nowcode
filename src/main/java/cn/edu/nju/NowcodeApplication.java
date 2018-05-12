04bbc2d2a456bcbd09aaf3703ccbf170d15caf42
congye6 <244053679@qq.com>
2018-05-12 17:38:11 +0800

    start

---
+++ src/main/java/cn/edu/nju/NowcodeApplication.java
@@ -0,0 +1,12 @@
+package cn.edu.nju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NowcodeApplication.class, args);
+	}
+}
